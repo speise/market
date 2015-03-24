f57a34f7a44d2e0c47c649381e1b336aa58682e3
Andrii Ilkiv <Andrii_Ilkiv@epam.com>
2015-03-24 13:52:11 +0000

    AI: Init commit

---
+++ src/main/java/org/service/UserService.java
@@ -0,0 +1,12 @@
+package org.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserService {
+	@RequestMapping("/user/all")
+	public String greeting() {
+		return new String("Response");
+	}
+}
