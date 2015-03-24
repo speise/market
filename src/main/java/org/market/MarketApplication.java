070497122a1e071fdfa9a6b2e0ff86cfdbff09e4
Volodymyr_Sydor1 <Volodymyr_Sydor1@epam.com>
2015-03-24 16:55:31 +0200

    VS: moved Userservice to service package set up default password added componentScan annotation

--- src/main/java/org/market/MarketApplication.java
+++ src/main/java/org/market/MarketApplication.java
@@ -2,8 +2,10 @@ package org.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.market.*")
 public class MarketApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/market/service/UserService.java
+++ src/main/java/org/market/service/UserService.java
@@ -1,9 +1,10 @@
-package org.service;
+package org.market.service;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class UserService {
 	@RequestMapping("/user/all")
 	public String greeting() {
