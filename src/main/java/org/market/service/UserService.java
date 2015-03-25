23e6b044251577fce028fb02747b8b16dd72d009
Volodymyr_Sydor1 <vs.nr.ua@gmail.com>
2015-03-25 14:12:04 +0200

    VS : added 2 simple views

--- src/main/java/org/market/service/UserService.java
+++ src/main/java/org/market/service/UserService.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class UserService {
 	@RequestMapping("/user/all")
 	public String greeting() {
-		return new String("Response");
+		return new String("All users list here");
 	}
 }
